1db59d3d0d3339f9dc2687c50c8082c58c5ee439
Alister Scott <alister.scott@gmail.com>
2011-03-16 09:01:04 +1000

    Cleaned up BaseStepDefinitions

--- SpecDriver/StepDefinitions/BaseStepDefinitions.cs
+++ SpecDriver/StepDefinitions/BaseStepDefinitions.cs
@@ -11,21 +11,6 @@ namespace Project1.StepDefinitions
     [Binding]
     public class BaseStepDefinitions
     {
-        //private const string CurrentPageKey = "current.page";
-
-        //protected dynamic CurrentPage
-        //{
-        //    get
-        //    {
-        //        return (BasePageModel)ScenarioContext.Current[CurrentPageKey];
-         //   }
-
-         //   set
-          //  {
-           //     ScenarioContext.Current[CurrentPageKey] = value;
-           // }
-        //}
-
         public static IWebDriver Driver { get; set; }
         protected GoogleHomePageModel GoogleHomePage { get; set; }
         protected GoogleSearchResultsPageModel GoogleSearchResultsPage { get; set; }
