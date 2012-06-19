045376a4cfca4819b753b72edb1e463703ebfdc9
Alister Scott <alister.scott@gmail.com>
2012-06-20 09:35:31 +1000

    Added auto generated files

--- SpecDriver/Pages/Etsy/EtsySearchResultsPageModel.cs
+++ SpecDriver/Pages/Etsy/EtsySearchResultsPageModel.cs
@@ -33,7 +33,7 @@ namespace Project1.Pages
 
         private IWebElement FirstResultLink
         {
-            get { return Driver.GetElement(FirstResultLinkSelector); }
+            get { return Driver.GetElement  (FirstResultLinkSelector); }
         }
 
         
